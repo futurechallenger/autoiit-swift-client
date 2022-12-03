b5472d902ad796450483f093dbcbd272c8efd5f4
bruce.li <unclecharlie@Uncles-MacBook-Pro.local>
2022-12-04 00:54:51 +0800

    add instrocution row for home page

---
+++ ios/ios/CommonView/IntroductionRow.swift
@@ -0,0 +1,20 @@
+//
+//  IntroductionRow.swift
+//  ios
+//
+//  Created by Uncle Charlie on 2022/12/4.
+//
+
+import SwiftUI
+
+struct IntroductionRow: View {
+    var body: some View {
+        Text("Find your favorite flows to save your time and money")
+    }
+}
+
+struct IntroductionRow_Previews: PreviewProvider {
+    static var previews: some View {
+        IntroductionRow()
+    }
+}
--- ios/ios/MyFlow/HomeView.swift
+++ ios/ios/MyFlow/HomeView.swift
@@ -8,16 +8,17 @@
 import SwiftUI
 
 struct HomeView: View {
-    var body: some View {
-        List {
-            ServiceInfoRow(text: "Hello World!")
-            ServiceInfoRow(text: "Autoiiiiiiiiiiiiiit!")
-        }
+  var body: some View {
+    List {
+      IntroductionRow()
+      ServiceInfoRow(text: "Hello World!")
+      ServiceInfoRow(text: "Autoiiiiiiiiiiiiiit!")
     }
+  }
 }
 
 struct HomeView_Previews: PreviewProvider {
-    static var previews: some View {
-        HomeView()
-    }
+  static var previews: some View {
+    HomeView()
+  }
 }
