3cade4ab99104c3202ea17f65e735460ad09e582
bruce.li <unclecharlie@Uncles-MacBook-Pro.local>
2022-12-08 00:09:41 +0800

    add gql in request for services view model

--- ios/ios/ViewModel/ServiceViewModel.swift
+++ ios/ios/ViewModel/ServiceViewModel.swift
@@ -9,13 +9,23 @@ import Foundation
 
 class ServiceViewModel: ObservableObject {
   @Published var serviceInfo: ServiceInfo? = nil
+  @Published var serviceInfoList: [ServiceInfo]? = nil
   
   func load(serviceId: Int) async -> ServiceInfo? {
     let serviceInfo = await HTTPService.request(of: ServiceInfo.self,
                                                 method: "GET",
                                                 headers: nil,
-                                                body: ["query": "", "variables": "", "operationName": ""])
-    if let ret = serviceInfo {
+                                                body: ["query": """
+                                                "query getService($serviceId: Int!) {
+                                                       service(id: $serviceId) {
+                                                          id
+                                                          name
+                                                        }
+                                                      }
+                                                """,
+                                                "variables": "{\"serviceId\": \(serviceId)}", "operationName": ""])
+    
+      if let ret = serviceInfo {
       self.serviceInfo = ret
     }
     
@@ -25,6 +35,23 @@ class ServiceViewModel: ObservableObject {
   // TODO: implement this method
   func loadList(serviceType: LoadSeviceType) async -> [ServiceInfo]? {
     
+    let res = await HTTPService.request(of: ServiceInfo.self,
+                                                method: "GET",
+                                                headers: nil,
+                                                body: ["query": """
+                                                        query getServices($type: String!) {
+                                                          servicesByType(serviceType: $type, userId:1) {
+                                                            id
+                                                            name
+                                                          }
+                                                         
+                                                        """,
+                                                       "variables": "{\"type\": \"RECOMMENDED\"}", "operationName": ""])
+    if let ret = res {
+      self.serviceInfoList = ret
+    }
+    
+    return serviceInfoList
   }
   
   enum LoadSeviceType {
